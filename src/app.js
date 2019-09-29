987902e4d2bbcc56cab96373dca509f50a2a2847
willowCeleste <willowceleste325@gmail.com>
2019-09-29 16:14:21 -0400

    Experimenting with Indego API

--- src/app.js
+++ src/app.js
@@ -1,6 +1,28 @@
-console.log("heyyy");
-
-$.get( "https://dkw6qugbfeznv.cloudfront.net", function( data ) {
-    console.log(data);
-  
+const init = () => {
+    $.get( "https://dkw6qugbfeznv.cloudfront.net", function( data ) {
+        populateStationsList(data.features);
   });
+}
+
+
+
+// get closest stations by coordinates
+const getStations = () => {
+    console.log('get closest stations');
+    $('#stations-list').append('<li>station</li>');
+}
+
+
+const populateStationsList = (stationsArray) => {
+    $(document).ready(() => {
+        for (var i = 0; i < stationsArray.length; i++) {
+            $('#stations-list').append(`<li>${stationsArray[i].properties.name}</li>`);
+        }
+        
+    });
+}
+
+
+populateStationsList();
+
+init();
